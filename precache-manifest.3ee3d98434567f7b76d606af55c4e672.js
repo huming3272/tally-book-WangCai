b7506b22a65b3439d6e3ec9b4901c162dee78cae
胡鸣 <9185847+huming3272@user.noreply.gitee.com>
2021-06-08 17:40:54 +0800

    deploy

---
+++ precache-manifest.3ee3d98434567f7b76d606af55c4e672.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d083f276618e52a8f05",
+    "url": "/tally-book-WangCai/css/app.3b0de492.css"
+  },
+  {
+    "revision": "f6bd069829c11cb09079",
+    "url": "/tally-book-WangCai/css/chunk-vendors.c470e980.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/tally-book-WangCai/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/tally-book-WangCai/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "14927cd10ebfa12b5873956948858bf1",
+    "url": "/tally-book-WangCai/img/QRcode.png"
+  },
+  {
+    "revision": "a75926aee1c423147025123106bdf576",
+    "url": "/tally-book-WangCai/index.html"
+  },
+  {
+    "revision": "605e64fd80a540234966",
+    "url": "/tally-book-WangCai/js/about.a0140021.js"
+  },
+  {
+    "revision": "4d083f276618e52a8f05",
+    "url": "/tally-book-WangCai/js/app.6d8ac72a.js"
+  },
+  {
+    "revision": "f6bd069829c11cb09079",
+    "url": "/tally-book-WangCai/js/chunk-vendors.4a4bb3ed.js"
+  },
+  {
+    "revision": "a7a2c52c0b639f56f7eb6e7c6990217c",
+    "url": "/tally-book-WangCai/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tally-book-WangCai/robots.txt"
+  }
+]);
